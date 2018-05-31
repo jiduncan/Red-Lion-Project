b616d85075ff77b8bfce6bef443f0f74797462ea
Johnny Duncan <38537795+Duncan-j@users.noreply.github.com>
2018-05-31 15:26:59 -0500

    connection and authentication working

---
+++ HTMLParser.java
@@ -0,0 +1,52 @@
+package htmlparser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import org.apache.commons.codec.binary.Base64;
+
+public class HTMLParser {
+
+	public static void main(String[] args) throws IOException {
+		//connects and tries to authenticate user @ 192.168.1.180
+		//using base64 encoding
+		try {
+			String webPage = "http://192.168.1.180";
+			String name = "java";
+			String password = "HTMLParser";
+
+			String authString = name + ":" + password;
+			System.out.println("auth string: " + authString);
+			byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
+			String authStringEnc = new String(authEncBytes);
+			System.out.println("Base64 encoded auth string: " + authStringEnc);
+
+			URL url = new URL(webPage);
+			URLConnection urlConnection = url.openConnection();
+			urlConnection.setRequestProperty("Authorization", "Basic " + authStringEnc);
+			InputStream is = urlConnection.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+
+			int numCharsRead;
+			char[] charArray = new char[1024];
+			StringBuffer sb = new StringBuffer();
+			while ((numCharsRead = isr.read(charArray)) > 0) {
+				sb.append(charArray, 0, numCharsRead);
+			}
+			String result = sb.toString();
+
+			System.out.println("*** BEGIN ***");
+			System.out.println(result);
+			System.out.println("*** END ***");
+			} 
+		catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+		catch (IOException e) {
+				e.printStackTrace();
+			}
+	}
+}
