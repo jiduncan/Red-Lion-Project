01f98d68cfda91460d7cac59f90dc1c9ae1969ab
jiduncan <32529196+jiduncan@users.noreply.github.com>
2018-08-13 13:33:18 -0500

    Updated timers

---
+++ FTPWeeklyBatch.bat
@@ -0,0 +1,6 @@
+@echo off
+timeout /t 60
+cd C:\Program Files (x86)\Red Lion Controls\Crimson 3.0\Utils
+websync -path C:\Users\johnny\Dropbox\RL_Logs\PTV_Logs 192.168.1.180 -user **** -pass Bf4elite123!
+echo finished File Transfer
+pause
